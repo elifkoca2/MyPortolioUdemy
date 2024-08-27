dafa123fad2eecaf1cc77475fe5667d9d8831033
elifkoca2 <koca24988@gmail.com>
2024-08-27 23:39:51 +0300

    Additions were made to the project

---
+++ MyPortolioUdemy/Controllers/ExperienceController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyPortolioUdemy.Controllers
+{
+    public class ExperienceController : Controller
+    {
+        public IActionResult ExperienceList()
+        {
+            return View();
+        }
+    }
+}
---
+++ MyPortolioUdemy/DAL/Entities/Message.cs
@@ -0,0 +1,13 @@
+﻿namespace MyPortolioUdemy.DAL.Entities
+{
+    public class Message
+    {
+        public int MessageId { get; set; }
+        public string NameSurname { get; set; }
+        public string Subject { get; set; }
+        public string Email { get; set; }
+        public string MessageDetails { get; set; }
+        public DateTime SendDate { get; set; }
+        public bool IssRead { get; set; }
+    }
+}
