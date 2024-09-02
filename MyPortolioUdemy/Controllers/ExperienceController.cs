9ea248a0bfdaa469b45608095a24a4272207739f
elifkoca2 <koca24988@gmail.com>
2024-09-02 22:20:00 +0300

    Additions were made to the project

--- MyPortolioUdemy/Controllers/ExperienceController.cs
+++ MyPortolioUdemy/Controllers/ExperienceController.cs
@@ -1,12 +1,29 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MyPortolioUdemy.DAL.Context;
+using MyPortolioUdemy.DAL.Entities;
 
 namespace MyPortolioUdemy.Controllers
 {
     public class ExperienceController : Controller
     {
+        MyPortfolioContext context = new MyPortfolioContext();
         public IActionResult ExperienceList()
         {
-            return View();
+            var values = context.Experiences.ToList();
+            return View(values);
+        }
+
+        [HttpGet]
+        public IActionResult CreateExperience()
+        {
+            return View(); 
+        }
+        [HttpPost]
+        public IActionResult CreateExperience(Experience experience)
+        {
+            context.Experiences.Add(experience);
+            context.SaveChanges();
+            return RedirectToAction("ExperienceList");
         }
     }
 }
---
+++ MyPortolioUdemy/ViewComponents/LayoutViewComponents/_LayoutHeadComponentPartial.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyPortolioUdemy.ViewComponents.LayoutViewComponents;
+
+namespace MyPortolioUdemy.ViewComponents.LayoutViewComponents
+{
+	public class _LayoutHeadComponentPartial:ViewComponent
+	{
+		public IViewComponentResult Invoke()
+		{
+			return View();
+		}
+	}
+}
